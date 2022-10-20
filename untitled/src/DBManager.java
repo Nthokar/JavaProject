3931e01226239958a41b30c72876edd1e7755aa2
Nthokar <zhvl03@gmail.com>
2022-10-20 13:55:57 +0500

    Add base structure of project

--- untitled/src/DBManager.java
+++
@@ -1,8 +0,0 @@
-public class DBManager {
-    // Класс управляющий взаимодействием с бд,
-    // занносит в нее значения об успеваемости студентах на курсах
-
-    public <T> void add(String table, String field, T value){
-        // Добавить в таблицу table в поле field значение value
-    }
-}
--- untitled/src/Student.java
+++
@@ -1,9 +0,0 @@
-public class Student extends Person {
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-    @Override
-    public int hashCode() { return super.hashCode(); }
-}
